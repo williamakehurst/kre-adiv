8578a2da606401109f4bb43539e1acf47b0ed7f4
William Akehurst <william@kasra.co>
2017-01-09 13:42:20 +0300

    Remove header content. Clean up code. Add contact section.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function(){
 	/* For Splash Animation */
-	var tl = new TimelineMax();
-	tl.fromTo('.splash__display', 1.5, {
+	var tl = new TimelineLite();
+	tl.fromTo('.splash__display, hr', 1.5, {
 		opacity: 0,
 		y: -100,
 	},{
@@ -15,42 +15,72 @@ $(function(){
 		opacity: 1,
 		x: 0,
 	},1.5);
-	tl.fromTo('.logo', 1, {
+	tl.fromTo('.splash__message', 1, {
 		opacity: 0,
-		y: -100,
-	},{
+		y: 100,
+	}, {
 		opacity: 1,
 		y: 0,
-	},2.5);
-	tl.fromTo('nav', 1, {
-		opacity: 0,
-		x: 200,
-	},{
-		opacity: 1,
-		x: 0,
-	},3.5);
-	$('.nav-item--projects').click(function() {
-		$('body').addClass('show--projects');
-		var projectsTl = new TimelineMax();
-		projectsTl.fromTo('.projects', 0.6, {
-			opacity: 0,
-			y: 100,
-		}, {
-			opacity: 1,
-			y: 0,
-		});
-		projectsTl.staggerTo('.projects__item, .title--project', 0.75, {
-			opacity: 1,
-		}, 0.5);
 	});
-	$('.projects .close').click(function() {
-		TweenMax.to('.projects', 0.2, {
-			opacity: 0,
-			y: 100,
-		});
-		TweenMax.to('.projects__item, .title--project', 0.1, {
-			opacity: 0,
-		});
-		$('body').removeClass('show--projects');
+	$(window).scroll(function(){
+		var windowHeight = $(window).height();
+		var scrollD = $(window).scrollTop();
+		var showFirstProject = windowHeight * 0.1;
+		var showSecondProject = windowHeight * 0.4;
+		var showThirdProject = windowHeight * 0.8;
+		var showContactInfo = windowHeight * 1.2;
+		if (scrollD > 30) {
+			TweenLite.to('.splash__message', 0.4, {
+				opacity: 0,
+				y: -30,
+			});
+		} else {
+			TweenLite.to('.splash__message', 0.4, {
+				opacity: 1,
+				y: 0,
+			});
+		}
+		if (scrollD > showFirstProject){
+			TweenLite.to('.business-cards', 0.5, {
+				opacity: 1,
+			});
+		} else if (scrollD < showFirstProject) {
+			TweenLite.to('.business-cards', 0.5, {
+				opacity: 0,
+			});
+		}
+		if (scrollD > showSecondProject){
+			TweenLite.to('.onboarding', 0.5, {
+				opacity: 1,
+			});
+			TweenLite.to('.splash__content', 0.5, {
+				opacity: 0,
+			});
+		} else if (scrollD < showSecondProject) {
+			TweenLite.to('.onboarding', 0.5, {
+				opacity: 0,
+			});
+			TweenLite.to('.splash__content', 0.5, {
+				opacity: 1,
+			});
+		}
+		if (scrollD > showThirdProject){
+			TweenLite.to('.rework', 0.5, {
+				opacity: 1,
+			});
+		} else if (scrollD < showThirdProject) {
+			TweenLite.to('.rework', 0.5, {
+				opacity: 0,
+			});
+		}
+		if (scrollD > showContactInfo) {
+			TweenLite.to('.about__content', 0.75, {
+				opacity: 1,
+			});
+		} else if (scrollD < showContactInfo) {
+			TweenLite.to('.about__content', 0.75, {
+				opacity: 0,
+			});
+		}
 	});
 });
